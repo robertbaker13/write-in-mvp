a99f303efa28252cc66d850ab2d4a11f9a98d1a2
Tom Lee <tlee418@g.ucla.edu>
2015-08-28 14:23:37 -0700

    Implement watching bugs discovered for the User class: 1. User can watch other Users multiple time 2. Need refractor for #add_watch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828184947) do
+ActiveRecord::Schema.define(version: 20150828202550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20150828184947) do
     t.string   "token"
     t.datetime "expires"
     t.integer  "district_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "watchings", force: :cascade do |t|
+    t.integer  "observer_id"
+    t.integer  "subject_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
