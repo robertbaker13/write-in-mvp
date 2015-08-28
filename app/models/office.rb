9324b09eaf59c1e011fb88f5a4139fad97a8de81
Tom Lee <tlee418@g.ucla.edu>
2015-08-28 15:42:50 -0700

    implement candidate table

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,3 +1,4 @@
 class Office < ActiveRecord::Base
   belongs_to :district
+  has_many :candidates
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828213030) do
+ActiveRecord::Schema.define(version: 20150828223527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candidates", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "office_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "districts", force: :cascade do |t|
     t.string   "name"
