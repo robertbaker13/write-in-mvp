041dd4af1a63d4bc01143393f13ec49d1ab137fb
name": null <email": null>
2015-08-28 15:44:25 -0700

    change a few lines of views and include bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(twitter_id: 3, token: "dff", expires: 5, district_id: 5)
