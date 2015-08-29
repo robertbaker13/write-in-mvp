5ef14a61a9ca38a23e8c9c956f6437e000df7be5
Lorraine C <email": null>
2015-08-28 21:03:25 -0700

    Revert "Update routes to nest districts under users"
    This reverts commit bce22a6436a4c2fa83096ede24e8dab7775701c8.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ActiveRecord::Base
   # Refractor later, and User can watch other user multiple time, need to fix
   def add_watch (user)
       Watching.create(observer: self, subject: user)
-    end
+  end
+
+  # Temporary filler method
+  def current_user
+    current_user = User.first
+  end
+
 end
 
