e44b1e9ece4ffdf0272b60a540574d6d6efadf13
Lorraine C <email": null>
2015-08-28 21:11:13 -0700

    Remove Static Page references
    Static pages were originally created as part of the report card functionality, but after further consideration the functionality may sit more suitably in the districts controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :districts
   resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-    get 'report' => 'static_pages#report'
+    get 'users/:id/districts/:id/report' => 'districts#report'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
