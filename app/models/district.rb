891cb7b4ca12ae7cb7b5f6321ac2f690bdc25edd
Lorraine Chan <lorrainechan@Lorraines-MacBook-Pro.local>
2015-08-29 16:04:14 -0700

    Clean up comments

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,16 +1,12 @@
 class District < ActiveRecord::Base
   has_many :children, class_name: "District",
                       foreign_key: "parent_id"
-  # Need to retrieving decendent district
-  # has_many :children, through: :children
   belongs_to :parent, class_name: "District",
                       foreign_key: "parent_id"
   has_many :users
   has_many :offices
 
-  districts = []
   def add_parents
-    # basecase - if district has no parents
     if self.parent == nil
       return District.where(id: self.id)
     else
@@ -18,12 +14,4 @@ class District < ActiveRecord::Base
     end
   end
 
-# result = []
-# Watching.where(observer: self).each do |watching|
-#   result << Endorsement.where(user: User
-
-#     watching.subject)
-# end
-
-# self.subjects.endorsements.group(:candidate).count
 end
