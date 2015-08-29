88cdac2efbb455c91a330d1116550d9ef8893a7c
Lorraine C <email": null>
2015-08-28 21:16:35 -0700

    Remove unneed action from DistrictsController and add report action

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,64 +1,7 @@
 class DistrictsController < ApplicationController
-  before_action :set_district, only: [:show, :edit, :update, :destroy]
 
-  # GET /districts
-  # GET /districts.json
-  def index
-    @districts = District.all
-  end
-
-  # GET /districts/1
-  # GET /districts/1.json
-  def show
-  end
-
-  # GET /districts/new
-  def new
-    @district = District.new
-  end
-
-  # GET /districts/1/edit
-  def edit
-  end
-
-  # POST /districts
-  # POST /districts.json
-  def create
-    @district = District.new(district_params)
-
-    respond_to do |format|
-      if @district.save
-        format.html { redirect_to @district, notice: 'District was successfully created.' }
-        format.json { render :show, status: :created, location: @district }
-      else
-        format.html { render :new }
-        format.json { render json: @district.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /districts/1
-  # PATCH/PUT /districts/1.json
-  def update
-    respond_to do |format|
-      if @district.update(district_params)
-        format.html { redirect_to @district, notice: 'District was successfully updated.' }
-        format.json { render :show, status: :ok, location: @district }
-      else
-        format.html { render :edit }
-        format.json { render json: @district.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /districts/1
-  # DELETE /districts/1.json
-  def destroy
-    @district.destroy
-    respond_to do |format|
-      format.html { redirect_to districts_url, notice: 'District was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  def report
+    @districts = District.find(params[:id]).add_parents
   end
 
   private
