d5edfbd8ec4076f34be77e3c3f7aad5dcd1a7586
Bobby Baker <robertbaker13@users.noreply.github.com>
2015-08-29 13:31:18 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,23 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+########################################################################################
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    @user = User.find(params[:id])
+    @watched_accounts = @user.watchings.map { |watching| watching.subject }
+    @endorsed_candidates = @user.endorsements.map { |endorsement| endorsement.candidate }
+    @watchers = Watching.where(subject: self).map { |watching| watching.observer }
+    @endorsements = Endorsement.where(candidate: Candidate.where(user: self))
+    #render profile
+  end
+########################################################################################
   # GET /users
   # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
 
   # GET /users/new
   def new
