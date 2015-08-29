c3dcbb68f09dc840aaf3ec79531ca62daf7d4998
Lorraine Chan <lorrainechan@Lorraines-MacBook-Pro.local>
2015-08-29 12:21:07 -0700

    Fix endorsement to candidate assocation

--- db/migrate/20150828224354_create_endorsements.rb
+++ db/migrate/20150828224354_create_endorsements.rb
@@ -1,8 +1,8 @@
 class CreateEndorsements < ActiveRecord::Migration
   def change
     create_table :endorsements do |t|
-      t.integer :endorser_id
-      t.integer :endorsee_id
+      t.integer :candidate_id
+      t.integer :user_id
 
       t.timestamps null: false
     end
