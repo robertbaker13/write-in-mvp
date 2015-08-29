7ad1acb75f1ba67dac4997be21faa49d76748ae8
Lorraine Chan <lorrainechan@Lorraines-MacBook-Pro.local>
2015-08-29 15:44:58 -0700

    Change schema for adding organization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ActiveRecord::Base
   belongs_to :district
   has_many :candidates
+  has_many :endorsements
 
-  has_many :observers, :class_name => "Watching", :foreign_key => "subject_id"
-  has_many :subjects, :class_name => "Watching", :foreign_key => "observer_id"
-
-  has_many :endorsers, :class_name => "Endorsement", :foreign_key => "endorsee_id"
-  has_many :endorsees, :class_name => "Endorsement", :foreign_key => "endorser_id"
-
+  has_one :organization
+  has_many :watchings
 
 
   def add_watch (user)
--- app/models/watching.rb
+++ app/models/watching.rb
@@ -1,11 +1,4 @@
 class Watching < ActiveRecord::Base
-    belongs_to :observer, class_name: "User"
-    belongs_to :subject, class_name: "User"
-
-    def list_subjects_for(user)
-      user.subjects.map do |person|
-        person.subject
-      end
-    end
-
+    belongs_to :user
+    belongs_to :organization
 end
