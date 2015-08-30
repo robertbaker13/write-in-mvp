7cd893e32b404d3598f1e68d71232ef9b0b20b8b
Robert Baker <robertbaker13@gmail.com>
2015-08-29 22:17:37 -0700

    add report card related methods to candidate, district, user, and office models

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,4 +1,12 @@
 class Office < ActiveRecord::Base
   belongs_to :district
   has_many :candidates
+
+  def rc_candidates(active_user)
+    result = self.candidates.map do |candidate|
+      { candidate: candidate, user: candidate.rc_user(active_user) }
+    end
+    result.select { |record| record.user }
+  end
+
 end
