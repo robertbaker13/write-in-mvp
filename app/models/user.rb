53a6f8fa774655709128f98a466b8b4439e678c1
Robert Baker <robertbaker13@gmail.com>
2015-08-29 17:40:26 -0700

    WIP on user model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,26 @@ class User < ActiveRecord::Base
   has_many :watchings
 
 
+  def home_info
+    #one object which we access in the few
+    User.find(id).district.add_parents.map { |district| {district => Office.where(district: district)} }
+
+  end
+
+
+
+
+
+  def profile_info
+    watched_accounts = current_user.watchings.map { |watching|  }
+  end
+
+
+  # @user = User.find(params[:id])
+  # @watched_accounts = @user.watchings.map{ |watching| watching.subject }
+  # @endorsed_candidates = @user.endorsements.map{ |endorsement| endorsement.candidate }
+  # @watchers = Watching.where(subject: self).map { |watching| watching.observer }
+
+
 end
 
