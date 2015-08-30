10c53dc0fe56272fc21382e60104aa945ac286ab
Tom Lee <tlee418@g.ucla.edu>
2015-08-30 11:06:10 -0700

    app can now get token

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
     get 'users/:user_id/districts/:id/report' => 'districts#report'
-    # match 'auth/:provider/callback' => 'sessions#create'
+    get 'auth/:provider/callback' => 'sessions#create'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
