4fdb331c2c5528de96026679b6b99eea61ce5637
name": null <email": null>
2015-08-30 12:06:00 -0700

    add profile_watched_users and profile_endorsed_candidates backend information user model methods

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -3,6 +3,7 @@ class Candidate < ActiveRecord::Base
   belongs_to :user
   has_many :endorsements
 
+  #checks user for a specific candidate
   def rc_user(active_user)
     p "self: #{self}"
     active_user.check_candidate(self.class)
