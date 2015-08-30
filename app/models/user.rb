76ee4952bf615808d2c059861b21a0be0408d95b
name": null <email": null>
2015-08-30 15:23:00 -0700

    add home_watched_users sorted and filtered by district

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,33 +8,45 @@ class User < ActiveRecord::Base
   has_one :twitter
 
   #top users to watch in user specific district calculation
-  def watched_users_score
-    endorsements = self.endorsements
-    followers = self.twitter.followers
-    if followers == false
-    sum_score = endorsements.compact.count
+  def home_watched_users_score
+    if self.twitter == nil || self.twitter.followers == nil || self.twitter.followers == [] || self.endorsements == [] #FL double checked these. However are there more eventualities?
+      sum_score = 1
     else
-    sum_score = endorsements.compact.count + followers.compact.count
+      endorsements = self.endorsements.map { |endorsement| endorsement }
+      followers = self.twitter.followers.map { |follower| follower }
+      sum_score = endorsements.compact.count + followers.compact.count
     end
   end
 
-  #checks that the listed users to watch are only those users that are not already endorsed by the viewer
-  def check_watched_users
+  #checks that the listed users to watch are only those users that are not already watched by the viewer
+  def home_check_watched_users
   end
 
+  #check that all information is district specific
+  def acceptable_district_watched_users
+    districts_for_user = self.district.add_parents
+    district_for_user_array = districts_for_user.map {|district| district.id}
+  end
+
+  #returns all users
+  def users
+    users_collection = User.all
+  end
 
   #top watch users suggestions for a specific user
   def home_login_users_to_watch
-    districts_for_user = self.district.add_parents
-    user_score = {}
-    users = User.all
-    test_array_of_scores = users.map { |user| user.users_score}
+    users_array = users.map {|user| user}
+    users_array = users_array.sort_by {|user| user.home_watched_users_score}
+    specific_district_user_array = users_array.select {|user| acceptable_district_watched_users.include?(user.district_id)}
+    # cut_off_users_array = specific_district_user_array[0..10]
   end
 
-  def candidates_score
+  #top candidates to endorse in user specific district calculation
+  def home_candidates_score
   end
 
-  def check_candidates
+  #checks that the listed candidates are only those candidates that are not already endorsed by the viewer
+  def home_check_candidates
   end
 
   #top candidate suggestions for a specific user
