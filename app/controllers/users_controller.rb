977865134feb11c70b6048a8343b5afd5d3ae5ac
user512 <tlee418@ucla.edu>
2015-08-31 12:31:47 -0700

    Initialize report card implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
 
   def report_card
     @reports = current_user.report_card
+    @watched_users = current_user.profile_watched_users
+    @twitter_profile_name = current_user.twitteruser.name
+    @twitter_handle = current_user.twitteruser.nickname
+    @twitter_profile_image = current_user.twitteruser.larger_image
   end
 
   def watch
