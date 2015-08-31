64639bce66a0650f898cf1cdd2001d1427618d77
Robert Baker <robertbaker13@gmail.com>
2015-08-30 21:14:17 -0700

    report card workin'

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,9 +1,8 @@
 class DistrictsController < ApplicationController
 
-  def report
-  end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_district
       @district = District.find(params[:id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def report_card
+    @reports = current_user.report_card
+  end
+
   # GET /users
   # GET /users.json
 
