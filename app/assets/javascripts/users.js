b92541d011b0c2548d99b1f0c40ca255e2b13228
Tom Lee <tlee418@g.ucla.edu>
2015-09-01 10:32:49 -0700

    fix unwatch ajax on profile page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,6 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 
-$(document).ready(function() {
-  unwatchListener();
-});
 
 $(function() {
     var availableTags = [
@@ -24,27 +21,3 @@ $(function() {
       source: availableTags
     });
   });
-
-var unwatchListener = function () {
-  console.log("listener running");
-  $(".button_to").on("submit", function(e) {
-    e.preventDefault();
-    var url = this.action;
-    var target = "." + this.children[1].id;
-
-    unwatch(url, target);
-  });
-
-  var unwatch = function(url, target){
-    console.log(url);
-    $.ajax({
-      url: url,
-      method: "post",
-      dataType: "json"
-    }).done(function() {
-      $(target).hide();
-    }).fail(function(){
-      console.log("fail");
-    });
-  }
-}
