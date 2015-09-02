8e17b04bf7e68985ae666435e8d297455082cddb
lchan912 <lchan912@gmail.com>
2015-09-02 10:32:58 -0700

    Extract ajax call to separate method

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,28 +3,36 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).ready(function(){
-  bindSearch();
+  bindSearchBySubmit();
+  bindSearchByButton();
 })
 
-
-
-var bindSearch = function(){
+var bindSearchBySubmit = function(){
   $('form.navbar-form').on('submit', function(event){
     event.preventDefault();
-
     var data = $("#peopleSearchBar").val();
-    $.ajax({
+    searchServer(data);
+  })
+}
+
+var bindSearchByButton = function(){
+  $('form.navbar-form').on('.glyphicon-search', function(event){
+    event.preventDefault();
+    var data = $("#peopleSearchBar").val();
+    searchServer(data);
+  })
+}
+
+var searchServer = function(data){
+      $.ajax({
       url: 'search',
       method: 'post',
       dataType: 'html',
       data: {search_input: data}
     }).done(function(responseData){
-      console.log('success');
-      console.log(responseData);
       displayResults(responseData);
       unendorseListener();
     })
-})
 }
 
 var displayResults = function(responseData){
