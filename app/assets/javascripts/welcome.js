4636ae30f38e1dc4de75accd2896b5c9f5f1f6e8
lchan912 <lchan912@gmail.com>
2015-09-02 10:26:44 -0700

    Move button related methods to user js

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -28,26 +28,5 @@ var bindSearch = function(){
 }
 
 var displayResults = function(responseData){
-  console.log('hi')
   $('.tab-content').html(responseData)
-}
-
-var unendorseListener = function(){
-  $("input.btn-danger.btn-xs.unendorse-button").on("click", function(e){
-    e.preventDefault();
-    var url = this.parentElement.action;
-    var $target = $(this).closest(".rcorners.bg-w.row.txt-c");
-    unendorse(url, $target);
-  })
-
-var unendorse = function(url, $target) {
-  $.ajax({
-    url: url,
-    method: "post"
-  }).done(function(){
-      $target.toggle("fast");
-    }).fail(function(){
-      console.log("fail");
-    })
-  }
 }
