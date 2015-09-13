5a9e62ee37ca14b4e7e66d77966b8ce5092c5fa2
Tom Lee <tlee418@g.ucla.edu>
2015-09-13 16:04:59 -0700

    add twitter newsfeed

--- app/controllers/twitterusers_controller.rb
+++ app/controllers/twitterusers_controller.rb
@@ -2,8 +2,6 @@ class TwitterusersController < ApplicationController
 
   def search
     @tusers = Twitteruser.search_db(params[:search_input])
-    p '*'*100
-    puts @tusers
 
     render @tusers.first
   end
--- app/models/twitteruser.rb
+++ app/models/twitteruser.rb
@@ -34,8 +34,8 @@ class Twitteruser < ActiveRecord::Base
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_KEY']
       config.consumer_secret     = ENV['TWITTER_SECRET']
-      config.access_token        = token
-      config.access_token_secret = secret
+      # config.access_token        = token
+      # config.access_token_secret = secret
     end
     client
   end
